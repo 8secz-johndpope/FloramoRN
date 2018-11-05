d0775b0b42d2af1bad78e8beb4c9791ee75a4e98
Luz <luzma_87@yahoo.com>
2018-11-04 22:36:30 -0500

    replaces flat list with section list

--- app/components/encyclopedia/EncyclopediaScreen.js
+++ app/components/encyclopedia/EncyclopediaScreen.js
@@ -1,6 +1,6 @@
 /* @flow */
 import React, { Component } from 'react';
-import { FlatList, View } from 'react-native';
+import { SectionList, View, Text } from 'react-native';
 import _ from 'lodash';
 import NthContainer from '../_common/NthHeader/NthContainer';
 import species from '../../../data/species';
@@ -8,31 +8,64 @@ import EncyclopediaItem from './EncyclopediaItem';
 import colors from '../../styles/colors';
 import type { Plant } from '../../../data/plantTypes';
 import appNavigation from '../../navigation/Routes';
+import NthText from '../_common/NthText/NthText';
 
 type Props = {
   navigation: Object
 };
 type State = {
-  sortedSpecies: Array<Plant>
+  sortedSpecies: Array<Object>
+};
+
+
+const sortPlant = (a, b) => {
+  const aFullName = `${a.genus} ${a.species}`;
+  const bFullName = `${b.genus} ${b.species}`;
+  if (aFullName > bFullName) {
+    return 1;
+  }
+  if (aFullName < bFullName) {
+    return -1;
+  }
+  return 0;
+};
+
+const getAlphabeticalObject = (sortedSpecies) => {
+  const tempSections = {};
+  sortedSpecies.forEach((plant) => {
+    const firstLetter = plant.genus[0];
+    if (!tempSections[firstLetter]) {
+      tempSections[firstLetter] = [];
+    }
+    tempSections[firstLetter].push(plant);
+  });
+  return tempSections;
+};
+
+const getAlphabeticalSections = (tempSections) => {
+  const sections = [];
+  Object.keys(tempSections).forEach((key) => {
+    const obj = {
+      title: key,
+      data: tempSections[key],
+    };
+    sections.push(obj);
+  });
+  return sections;
+};
+
+const makeAlphabeticalSections = () => {
+  const sortedSpecies = _.cloneDeep(species);
+  sortedSpecies.sort((a, b) => sortPlant(a, b));
+  const tempSections = getAlphabeticalObject(sortedSpecies);
+  return getAlphabeticalSections(tempSections);
 };
 
 class EncyclopediaScreen extends Component<Props, State> {
   constructor(props: any) {
     super(props);
-    const sortedSpecies = _.cloneDeep(species);
-    sortedSpecies.sort((a, b) => {
-      const aFullName = `${a.genus} ${a.species}`;
-      const bFullName = `${b.genus} ${b.species}`;
-      if (aFullName > bFullName) {
-        return 1;
-      }
-      if (aFullName < bFullName) {
-        return -1;
-      }
-      return 0;
-    });
     this.state = {
-      sortedSpecies,
+      sortedSpecies: makeAlphabeticalSections(),
     };
   }
 
@@ -47,11 +80,20 @@ class EncyclopediaScreen extends Component<Props, State> {
   render() {
     const { navigation } = this.props;
     const { sortedSpecies } = this.state;
+
     return (
       <NthContainer onPress={() => navigation.openDrawer()} i18nHeader="navigation.title.encyclopedia" noPadding>
         <View style={{ flex: 1 }}>
-          <FlatList
-            data={sortedSpecies}
+          <SectionList
+            renderSectionHeader={({ section: { title } }) => (
+              <NthText
+                text={title}
+                weight="black"
+                size="title"
+                style={{ backgroundColor: colors.primary200, padding: 12 }}
+              />
+            )}
+            sections={sortedSpecies}
             renderItem={({ item }) => (
               <EncyclopediaItem
                 plant={item}
