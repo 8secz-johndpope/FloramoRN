9862ab04e4c39e4db74a4ed0a476b6b1f9c5f48e
Luz <luzma_87@yahoo.com>
2018-11-18 12:05:42 -0500

    adds missing places translation file

---
+++ assets/translations/places.js
@@ -0,0 +1,22 @@
+const places = {
+  azuay: 'Azuay',
+  bolivar: 'Bolívar',
+  canar: 'Cañar',
+  carchi: 'Carchi',
+  chimborazo: 'Chimborazo',
+  cotopaxi: 'Cotopaxi',
+  eloro: 'El Oro',
+  esmeraldas: 'Esmeraldas',
+  imbabura: 'Imbabura',
+  loja: 'Loja',
+  losrios: 'Los Ríos',
+  moronasantiago: 'Morona Santiago',
+  napo: 'Napo',
+  pastaza: 'Pastaza',
+  pichincha: 'Pichincha',
+  sucumbios: 'Sucumbíos',
+  tungurahua: 'Tungurahua',
+  zamorachinchipe: 'Zamora Chinchipe',
+};
+
+export default places;
